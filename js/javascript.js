3929d0694659f6b48b5e846ed2ac81fa40b24217
Kertu Kipper <phkel@users.noreply.github.com>
2018-03-09 11:48:42 +0200

    Add mainpage

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+// Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("firstname", "");
+    // Retrieve
+    document.getElementById("player").innerHTML = localStorage.getItem("firstname");
+} else {
+    document.getElementById("player").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
