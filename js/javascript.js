ebede09702b426ad3ea7c858d4f507f527f8f268
Krislyn Luks <krsln@tlu.ee>
2018-03-25 14:08:17 +0300

    button for dark mode

--- js/javascript.js
+++ js/javascript.js
@@ -7,3 +7,25 @@ if (typeof(Storage) !== "undefined") {
 } else {
     document.getElementById("player").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
+
+
+// button for dark mode (krislyn)
+
+
+.button {
+    background-color: pink;
+    border: none;
+    color: white;
+    width: 3%;
+    height: 0;
+    padding-bottom: 3%;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 13px;
+    position:fixed;
+    bottom:0;
+
+}
+
+.button{border-radius: 100%;}
